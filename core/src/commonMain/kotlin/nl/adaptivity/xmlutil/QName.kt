bf7d0005f18846e9fce97756bc5ad8030ac25e74
Paul de Vrieze <paul.devrieze@gmail.com>
2022-06-22 15:14:26 +0100

    Fix compilation with 1.7.0

--- core/src/commonMain/kotlin/nl/adaptivity/xmlutil/QName.kt
+++ core/src/commonMain/kotlin/nl/adaptivity/xmlutil/QName.kt
@@ -69,7 +69,7 @@ public object QNameSerializer : KSerializer<QName> {
                 2 -> prefix = decodeStringElement(descriptor, 2)
             }
         }
-        return QName(namespace, localPart, prefix)
+        QName(namespace, localPart, prefix)
     }
 
     @OptIn(ExperimentalSerializationApi::class)
